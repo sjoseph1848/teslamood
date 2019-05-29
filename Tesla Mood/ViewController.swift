a0e8b538e641ab54d9f82b942e22f6f610499074
Samuel Joseph <samueljoseph@Samuels-MBP.frontierlocal.net>
2019-05-28 21:35:42 -0500

    first commit

---
+++ Tesla Mood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tesla Mood
+//
+//  Created by Samuel Joseph on 5/28/19.
+//  Copyright © 2019 Samuel Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
